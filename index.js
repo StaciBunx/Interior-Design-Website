578c07c7b3134e3cac0579a77450fb15b738d3d3
Jeiojey <signofaries@gmail.com>
2023-10-26 16:18:55 +0300

    feat: add vue for articles

---
+++ index.js
@@ -0,0 +1,44 @@
+new Vue({
+    el: "#app",
+    data: {
+        articles: [
+            {
+                src: "./images/index/blog-1.jpg",
+                tag: "Kitchen Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "26 December, 2022",
+            },
+            {
+                src: "./images/index/blog-2.jpg",
+                tag: "Living Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "22 December, 2022",
+            },
+            {
+                src: "./images/index/blog-3.jpg",
+                tag: "Interior Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "25 December, 2022",
+            },
+            {
+                src: "./images/index/blog-1.jpg",
+                tag: "Kitchen Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "26 December, 2022",
+            },
+            {
+                src: "./images/index/blog-2.jpg",
+                tag: "Living Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "22 December, 2022",
+            },
+            {
+                src: "./images/index/blog-3.jpg",
+                tag: "Interior Design",
+                heading: "Let’s Get Solution For Building Construction Work",
+                date: "25 December, 2022",
+            },
+        ]
+    }
+
+})
